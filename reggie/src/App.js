084bbf6d15518afaa1a843a1f1d6b34fe8c5bce5
Marine809 <rthorpesr@verizon.net>
2019-08-24 22:00:42 -0400

    updates

--- reggie/src/App.js
+++ reggie/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import createhomework from "./components/createhomework.js";
 import edithomework from "./components/edithomework.js";
@@ -12,7 +13,22 @@ class App extends Component {
      return (
        <Router>
          <div className="Container">
-             <h2> ----------             Hello Reggie</h2>
+
+             <nav className="navbar nabbar-expan-lg navbar-light bg-light">
+               <Link to="/" className="navbar-bran">Reggie Mern Stack app</Link>
+               <div className="collapse nav-collapse">
+                  <ul className="navbar-nav mr-auto">
+                    <li className="navbar-item">
+                       <Link to="/" className="nav-link">Homework</Link>
+                    </li>
+                    <li className="navbar-item">
+                       <Link to="/create" className="nav-link">Create Homework</Link>
+                    </li>
+                  </ul>  
+               </div>   
+             </nav>
+        
+             <h2>Hello Reggie</h2>
              <Route path="/homework" exact component={homeworklist} />
              <Route path="/edit/:id" component={edithomework} />
             <Route path="/create" component={createhomework} />
